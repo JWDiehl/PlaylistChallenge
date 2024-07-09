615195cdd7af488f8ce2174dd83dce316d012818
Jon <jon@Zipcoders-MacBook-Pro-99.local>
2024-07-09 17:42:45 -0400

    Music Finished

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -5,10 +5,30 @@ public class Music {
     private String[] playList;
 
     public Music(String[] playList){
+
         this.playList = playList;
     }
 
     public Integer selection(Integer startIndex, String selection){
-        return null;
+        //Your function
+        // must return an integer denoting the minimum number of button presses needed to switch from song sk to song q.
+        // This is printed to stdout by the locked stub code in your editor.
+
+
+        //Initialize length of playlist = n
+        int n = playList.length;
+        //Initialize song Q index
+        int songQIndex = -1;
+
+        //For loop to iterate through song playlist (array list)
+        for (int i = 0; i < n; i++) {
+            if (playList[i].equals(selection)) {
+                break;
+            }
+        }
+        int buttonSkipsUp = (songQIndex - startIndex + n) % n;
+        int buttonSkipsDown = (startIndex - songQIndex + n) % n;
+
+        return Math.min(buttonSkipsUp, buttonSkipsDown);
     }
 }
