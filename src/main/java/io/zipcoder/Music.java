c4f72d484db16daf557b7d17718b498a71ef8802
Jon <jon@Zipcoders-MacBook-Pro-99.local>
2024-07-10 08:56:08 -0400

    Finished

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -9,6 +9,7 @@ public class Music {
         this.playList = playList;
     }
 
+    //Method starts here
     public Integer selection(Integer startIndex, String selection){
         //Your function
         // must return an integer denoting the minimum number of button presses needed to switch from song sk to song q.
@@ -17,7 +18,7 @@ public class Music {
 
         //Initialize length of playlist = n
         int n = playList.length;
-        //Initialize song Q index
+        //Initialize song Q index at -1 bc when we get to index of 0, the playlist starts
         int songQIndex = -1;
 
         //For loop to iterate through song playlist (array list)
@@ -26,9 +27,14 @@ public class Music {
                 break;
             }
         }
+        //Number of button presses needed to move from start index to songQIndex in forward direction
+        // % n handles the circular nature of playlists
         int buttonSkipsUp = (songQIndex - startIndex + n) % n;
+
+        //Number of button presses needed to move from start index to songQIndex in backward direction
         int buttonSkipsDown = (startIndex - songQIndex + n) % n;
 
+        //Return minimum number of getting to songQIndex (its either buttonskipsup or button skipsdown)
         return Math.min(buttonSkipsUp, buttonSkipsDown);
     }
 }
